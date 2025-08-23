0a9b4b889c4641858b1f2a9fa79ae07bab9865a5
shandikadav <shandikadavid.14@gmail.com>
2025-08-23 17:39:34 +0700

    feat: add events menu on dashboard

---
+++ app/Filament/Resources/Events/Schemas/EventForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Events\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class EventForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                FileUpload::make('image_url')
+                    ->image()
+                    ->required(),
+            ]);
+    }
+}
